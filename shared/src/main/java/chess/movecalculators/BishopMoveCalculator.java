5a15e5c45c23b73da8138e25d818d1c5f99b0d42
sean titensor <seantitensor12000@gmail.com>
2026-01-07 14:45:15 -0700

    completed bishop move calculator, and the interface piecemovecalculator

--- shared/src/main/java/chess/movecalculators/BishopMoveCalculator.java
+++ shared/src/main/java/chess/movecalculators/BishopMoveCalculator.java
@@ -1,5 +1,53 @@
 package chess.movecalculators;
 
-public class BishopMoveCalculator {
-    
+import java.util.ArrayList;
+import java.util.Collection;
+
+import chess.ChessBoard;
+import chess.ChessMove;
+import chess.ChessPiece;
+import chess.ChessPosition;
+
+
+public class BishopMoveCalculator implements PieceMoveCalculator {
+
+    @Override
+    public Collection<ChessMove> pieceMoves(ChessBoard board, ChessPosition myPosition){
+        ArrayList<ChessMove> moves = new ArrayList<>();
+        ChessPiece myPiece = board.getPiece(myPosition);
+
+        int[][] directions = {
+            {1,1}, 
+            {1,-1}, 
+            {-1,1}, 
+            {-1,-1}
+        };
+
+        for (int[] direction : directions) {
+            int row = myPosition.getRow();
+            int col = myPosition.getColumn();
+
+            while (true) {
+                row += direction[0];
+                col += direction[1];
+
+                if (row < 1 || row > 8 || col < 1 || col > 8) { break; }
+
+                ChessPosition newPosition = new ChessPosition(row, col);
+                ChessPiece pieceAtNewPosition = board.getPiece(newPosition);
+
+                if (pieceAtNewPosition == null) {
+                    moves.add(new ChessMove(myPosition, newPosition, null));
+                }
+                else if (pieceAtNewPosition.getTeamColor() != myPiece.getTeamColor()) {
+                    moves.add(new ChessMove(myPosition, newPosition, null));
+                    break;
+                }
+                else {
+                    break;
+                }
+            }
+        }
+        return moves;
+    }
 }
--- shared/src/main/java/chess/movecalculators/PieceMoveCalculator.java
+++ shared/src/main/java/chess/movecalculators/PieceMoveCalculator.java
@@ -1,5 +1,12 @@
 package chess.movecalculators;
 
+import java.util.Collection;
+
+import chess.ChessBoard;
+import chess.ChessMove;
+import chess.ChessPosition;
+
 public interface PieceMoveCalculator {
-    
+
+    Collection<ChessMove> pieceMoves(ChessBoard board, ChessPosition myPosition);
 }
