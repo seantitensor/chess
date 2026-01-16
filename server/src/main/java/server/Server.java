fdfddcab582516ccf8d636672d97d6985f7a61cf
sean titensor <seantitensor12000@gmail.com>
2026-01-16 12:19:12 -0700

    added the handler functions to the sever class, just placeholders for now.

--- server/src/main/java/server/Server.java
+++ server/src/main/java/server/Server.java
@@ -1,9 +1,7 @@
 package server;
 
-import handlers.ClearHandler;
-import handlers.GameHandler;
-import handlers.UserHandler;
 import io.javalin.Javalin;
+import io.javalin.http.Context;
 
 public class Server {
 
@@ -13,28 +11,25 @@ public class Server {
         javalin = Javalin.create(config -> config.staticFiles.add("web"));
 
         // Register your endpoints and exception handlers here.
-        UserHandler userHandler = new UserHandler();
-        GameHandler gameHandler = new GameHandler();
-        ClearHandler clearHandler = new ClearHandler();
 
         // user endpoints
-        javalin.post("/user",userHandler::register);
-        javalin.post("/user",userHandler::login);
-        javalin.delete("/user", userHandler::logout);
+        javalin.post("/user", this::register);
+        javalin.post("/user", this::login);
+        javalin.delete("/user", this::logout);
 
         // game endpoints
-        javalin.get("/game", gameHandler::listGames);
-        javalin.post("/game", gameHandler::createGame);
-        javalin.put("/game", gameHandler::joinGame);
+        javalin.get("/game", this::listGames);
+        javalin.post("/game", this::createGame);
+        javalin.put("/game", this::joinGame);
 
         // clear endpoints
-        javalin.delete("/db", clearHandler::clear);
+        javalin.delete("/db", this::clear);
 
         // exception handling
         javalin.exception(Exception.class, (e, ctx) -> {
             ctx.status(500);
             ctx.json("{\"message\": \"Error: " + e.getMessage() + "\"}");
-            
+
         });
     }
 
@@ -45,5 +40,36 @@ public class Server {
 
     public void stop() {
         javalin.stop();
+    }
+
+    // clear handler
+    private void clear(Context ctx) {
+        ctx.status(200).json("{\"message\": cleared application succesfully}");
+    }
+
+    //game handler
+    private void listGames(Context ctx) {
+        ctx.status(200).json("{\"message\": found games}");
+    }
+
+    private void createGame(Context ctx) {
+        ctx.status(200).result("game Created");
+    }
+
+    private void joinGame(Context ctx) {
+        ctx.status(200).result("game joined sucesfully");
+    }
+
+    //user handler
+    private void login(Context ctx) {
+        ctx.status(200).json("{\"message\": Login successful}");
+    }
+
+    private void register(Context ctx) {
+        ctx.status(200).result("User created");
+    }
+
+    private void logout(Context ctx) {
+        ctx.status(200).result("Logout succesful");
     }
 }
