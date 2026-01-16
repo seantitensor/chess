328ef04d66c57f14e4b3dd87b84742eaeefb4796
sean titensor <seantitensor12000@gmail.com>
2026-01-16 12:16:18 -0700

    added the endpoints to the javalin object

--- server/src/main/java/server/Server.java
+++ server/src/main/java/server/Server.java
@@ -1,6 +1,9 @@
 package server;
 
-import io.javalin.*;
+import handlers.ClearHandler;
+import handlers.GameHandler;
+import handlers.UserHandler;
+import io.javalin.Javalin;
 
 public class Server {
 
@@ -10,7 +13,29 @@ public class Server {
         javalin = Javalin.create(config -> config.staticFiles.add("web"));
 
         // Register your endpoints and exception handlers here.
+        UserHandler userHandler = new UserHandler();
+        GameHandler gameHandler = new GameHandler();
+        ClearHandler clearHandler = new ClearHandler();
 
+        // user endpoints
+        javalin.post("/user",userHandler::register);
+        javalin.post("/user",userHandler::login);
+        javalin.delete("/user", userHandler::logout);
+
+        // game endpoints
+        javalin.get("/game", gameHandler::listGames);
+        javalin.post("/game", gameHandler::createGame);
+        javalin.put("/game", gameHandler::joinGame);
+
+        // clear endpoints
+        javalin.delete("/db", clearHandler::clear);
+
+        // exception handling
+        javalin.exception(Exception.class, (e, ctx) -> {
+            ctx.status(500);
+            ctx.json("{\"message\": \"Error: " + e.getMessage() + "\"}");
+            
+        });
     }
 
     public int run(int desiredPort) {
