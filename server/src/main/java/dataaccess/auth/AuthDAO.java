ecc661879b223d7a0e70e949a0d89a4c0cb22c79
sean titensor <seantitensor12000@gmail.com>
2026-01-17 20:22:51 -0700

    implemented the authDAO and localAUTHDao

--- server/src/main/java/dataaccess/auth/AuthDAO.java
+++ server/src/main/java/dataaccess/auth/AuthDAO.java
@@ -1,8 +1,15 @@
 package dataaccess.auth;
 
+import dataaccess.DataAccessException;
+import model.AuthData;
+
 public interface AuthDAO {
     // create auth Token
+    void createAuth(String authToken, String username) throws DataAccessException;
     // get auth token
+    AuthData getAuthData(String authToken) throws DataAccessException;
     // delete auth token
+    void deleteAuth(String authToken) throws DataAccessException;
     // clear auth tokens
+    void clearAuth();
 }
--- server/src/main/java/dataaccess/auth/LocalAuthDAO.java
+++ server/src/main/java/dataaccess/auth/LocalAuthDAO.java
@@ -1,5 +1,33 @@
 package dataaccess.auth;
 
+import java.util.HashMap;
+import java.util.Map;
+
+import dataaccess.DataAccessException;
+import model.AuthData;
+
 public class LocalAuthDAO implements AuthDAO {
+    private final Map<String, AuthData> authDatas = new HashMap<>();
+
+    @Override
+    public void createAuth(String authToken, String username) throws DataAccessException {
+        authDatas.put(authToken, new AuthData(username, authToken));
+    }
+
+    @Override
+    public AuthData getAuthData(String authToken) throws DataAccessException {
+        return authDatas.get(authToken);
+    }
+
+    @Override
+    public void deleteAuth(String authToken) throws DataAccessException {
+        authDatas.remove(authToken);
+    }
+
+    @Override
+    public void clearAuth() {
+        authDatas.clear();
+    }
+
     
 }
