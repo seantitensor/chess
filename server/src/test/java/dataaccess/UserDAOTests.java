4a544d28f1f33ecb3deb147826ef3b1f44d8c15b
sean titensor <seantitensor12000@gmail.com>
2026-02-12 19:06:19 -0700

    finished user dao tests. all tests are passing

--- server/src/test/java/dataaccess/UserDAOTests.java
+++ server/src/test/java/dataaccess/UserDAOTests.java
@@ -4,9 +4,17 @@
  */
 
 package dataaccess;
-
+import static org.junit.jupiter.api.Assertions.assertDoesNotThrow;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.junit.jupiter.api.Assertions.assertNull;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Order;
 import org.junit.jupiter.api.Test;
-import static org.junit.jupiter.api.Assertions.*;
+
+import dataaccess.user.SqlUserDAO;
+import model.UserData;
  
 /**
  *
@@ -17,4 +25,66 @@ public class UserDAOTests {
      public UserDAOTests() {
     }
 
+
+    private SqlUserDAO dao = new SqlUserDAO();
+
+    @BeforeEach
+    void setup() {
+        dao.clear();
+    }
+
+    @Test
+    @DisplayName("positive create")
+    @Order(1)
+    void positiveCreateUserTest() {
+        UserData user = new UserData("sean","seanrocks","seanemail");
+        dao.createUser(user);
+        assertNotNull(dao.getUser("sean"));
+    }
+
+    @Test
+    @DisplayName("negative create")
+    @Order(2)
+    void negativeCreateAuthTest() {
+        UserData user = new UserData("sean","seanrocks","seanemail");
+        dao.createUser(user);
+        UserData user1 = new UserData("sean","seanrocks","seanemail");
+        assertThrows(DataAccessException.class, ()-> dao.createUser(user1));
+    }
+
+    @Test
+    @DisplayName("positive get user")
+    @Order(3)
+    void positiveGetUserTest() {
+        UserData user = new UserData("sean","seanrocks","seanemail");
+        dao.createUser(user);
+        assertNotNull(dao.getUser("sean"));
+    }
+
+    @Test
+    @DisplayName("Negative get user")
+    @Order(4)
+    void negativeGetAuthTest() {
+        UserData user = new UserData("sean","seanrocks","seanemail");
+        dao.createUser(user);
+        assertNull(dao.getUser("seanrocks"));
+    }
+
+    @Test
+    @DisplayName("positive clear user")
+    @Order(5)
+    void positiveClearUserTest() {
+        UserData user = new UserData("sean","seanrocks","seanemail");
+        dao.createUser(user);
+        dao.clear();
+        assertNull(dao.getUser("sean"));
+
+    }
+
+    @Test
+    @DisplayName("Negative clear auth")
+    @Order(8)
+    void negativeClearAuthTest() {
+        assertDoesNotThrow(()->dao.clear());
+    }
 }
