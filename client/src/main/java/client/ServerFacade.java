99371143784a16c7d69b1956d86c717f3923dc79
sean titensor <seantitensor12000@gmail.com>
2026-02-27 10:00:41 -0700

    added some inital files from pet shop and the starter code.

---
+++ client/src/main/java/client/ServerFacade.java
@@ -0,0 +1,95 @@
+package client;
+
+import com.google.gson.Gson;
+
+import exception.ResponseException;
+import model.*;
+
+import java.net.*;
+import java.net.http.*;
+import java.net.http.HttpRequest.BodyPublisher;
+import java.net.http.HttpRequest.BodyPublishers;
+import java.net.http.HttpResponse.BodyHandlers;
+
+import request.LoginRequest;
+
+public class ServerFacade {
+    private final HttpClient client = HttpClient.newHttpClient();
+    private final String serverUrl;
+
+    public ServerFacade(String url) {
+        serverUrl = url;
+    }
+
+    public UserData login(LoginRequest req) throws Exception {
+        var request = buildRequest("POST", "/session", UserData);
+        var response = sendRequest(request);
+        return handleResponse(response, Pet.class);
+    }
+
+    public void deletePet(int id) throws ResponseException {
+        var path = String.format("/pet/%s", id);
+        var request = buildRequest("DELETE", path, null);
+        var response = sendRequest(request);
+        handleResponse(response, null);
+    }
+
+    public void deleteAllPets() throws ResponseException {
+        var request = buildRequest("DELETE", "/pet", null);
+        sendRequest(request);
+    }
+
+    public PetList listPets() throws ResponseException {
+        var request = buildRequest("GET", "/pet", null);
+        var response = sendRequest(request);
+        return handleResponse(response, PetList.class);
+    }
+
+    private HttpRequest buildRequest(String method, String path, Object body) {
+        var request = HttpRequest.newBuilder()
+                .uri(URI.create(serverUrl + path))
+                .method(method, makeRequestBody(body));
+        if (body != null) {
+            request.setHeader("Content-Type", "application/json");
+        }
+        return request.build();
+    }
+
+    private BodyPublisher makeRequestBody(Object request) {
+        if (request != null) {
+            return BodyPublishers.ofString(new Gson().toJson(request));
+        } else {
+            return BodyPublishers.noBody();
+        }
+    }
+
+    private HttpResponse<String> sendRequest(HttpRequest request) throws ResponseException {
+        try {
+            return client.send(request, BodyHandlers.ofString());
+        } catch (Exception ex) {
+            throw new ResponseException(ResponseException.Code.ServerError, ex.getMessage());
+        }
+    }
+
+    private <T> T handleResponse(HttpResponse<String> response, Class<T> responseClass) throws ResponseException {
+        var status = response.statusCode();
+        if (!isSuccessful(status)) {
+            var body = response.body();
+            if (body != null) {
+                throw ResponseException.fromJson(body);
+            }
+
+            throw new ResponseException(ResponseException.fromHttpStatusCode(status), "other failure: " + status);
+        }
+
+        if (responseClass != null) {
+            return new Gson().fromJson(response.body(), responseClass);
+        }
+
+        return null;
+    }
+
+    private boolean isSuccessful(int status) {
+        return status / 100 == 2;
+    }
+}
